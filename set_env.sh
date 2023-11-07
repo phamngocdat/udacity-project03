35b9cf9aa972abbd9b0dcaad0050bcb2d6d741ef
phamngocdat <Ngocdat199614@gmail.com>
2023-11-07 19:31:16 +0700

    circle ci

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-1.c5dc0vgocbow.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=project3-udacity.ca2kx89geyqh.ap-southeast-1.rds.amazonaws.com
 export POSTGRES_DB=udacity
-export AWS_BUCKET=arn:aws:s3:::udacity-project3-datpn-bucket
-export AWS_REGION=us-east-1
+export AWS_BUCKET=udacity-project3-datpn-bucket1
+export AWS_REGION=ap-southeast-1
 export AWS_PROFILE=default
 export JWT_SECRET=test
 export URL=http://localhost:8100
