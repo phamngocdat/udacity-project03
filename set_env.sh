31a3feb32788dee829701f32047bf296b13bf14e
dat <datht@example.com>
2023-11-07 16:19:06 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.c5dc0vgocbow.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udacity
+export AWS_BUCKET=arn:aws:s3:::udacity-project3-datpn-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=test
+export URL=http://localhost:8100
